f55ecaa033945e4f8e05db33de11988376be6752
Marius <mettelsi@th-brandenburg.de>
2022-04-11 15:07:05 +0000

    remake

---
+++ Greeter.java
@@ -0,0 +1,11 @@
+package myproject
+public class Greeter {
+String name;
+public Greeter(String name) {
+this.name = name;
+}
+public String greet() {
+return "Hello " + name + ", nice to meet you!");
+}
+}
+
